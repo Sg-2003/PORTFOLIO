b02c0b5af11831c878608e71a0ed248575cd4041
SUKUMAR GOPE <138880865+Sg-2003@users.noreply.github.com>
2025-11-24 00:42:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname){
@@ -32,39 +31,4 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxWLgD2gNObAQRjz2oMtK
         form.reset()
       })
       .catch(error => console.error('Error!', error.message))
-=======
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-var sidemenu = document.getElementById("sidemenu");
-function openmenu(){
-    sidemenu.style.right = "0";
-}
-function closemenu(){
-    sidemenu.style.right = "-200px";
-}
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxWLgD2gNObAQRjz2oMtK1jcrLgBLAwL8vwAMiO8JLext89oM5l9algpJI02hqpehiH/exec'
-  const form = document.forms['submit-to-google-sheet']
-   const msg = document.getElementById("msg")
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML = "Message sent successfully"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
->>>>>>> 97d01af560ece01a9ab9df6408420e536ad4da05
   })
